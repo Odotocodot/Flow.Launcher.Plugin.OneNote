20919ee7341d97095ffa9b044e43f490f9a2efb3
Odotocodot <48138990+Odotocodot@users.noreply.github.com>
2023-04-30 21:16:14 +0100

    Finished Settings View

--- ViewModels/SettingsViewModel.cs
+++ ViewModels/SettingsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 
 namespace Flow.Launcher.Plugin.OneNote.ViewModels
 {
@@ -9,12 +10,32 @@ namespace Flow.Launcher.Plugin.OneNote.ViewModels
         public SettingsViewModel(Settings settings)
         {
             Settings = settings;
+            ResetTimeoutCommand = new ResetCommand(this);
         }
         public Settings Settings { get; init; }
+
         public IEnumerable<int> DefaultRecentCountOptions => Enumerable.Range(1, 16);
 
-        public IEnumerable<TimeType> TimeTypes => Enum.GetValues<TimeType>().Cast<TimeType>();
+        public ICommand ResetTimeoutCommand { get; init; }
 
-        public int SmallChange => 1;
+        private class ResetCommand : ICommand
+        {
+            private readonly SettingsViewModel viewModel;
+
+            public ResetCommand(SettingsViewModel viewModel)
+            {
+                this.viewModel = viewModel;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter) => true;
+
+            public void Execute(object parameter)
+            {
+                viewModel.Settings.COMReleaseTimeout = 10000;
+                viewModel.Settings.TimeType = TimeType.milliseconds;
+            }
+        }
     }
 }
