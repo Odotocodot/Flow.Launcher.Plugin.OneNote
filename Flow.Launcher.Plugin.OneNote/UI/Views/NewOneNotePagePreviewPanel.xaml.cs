cbbf46300df01b50f9e10a08a8216ef6ae6b62f1
Odotocodot <48138990+Odotocodot@users.noreply.github.com>
2024-06-06 15:11:42 +0100

    Rename images to match icon theme rather than plugin theme
    It makes sense right? Right?

---
+++ Flow.Launcher.Plugin.OneNote/UI/Views/NewOneNotePagePreviewPanel.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Flow.Launcher.Plugin.OneNote.UI.Views;
+
+public partial class NewOneNotePagePreviewPanel : UserControl
+{
+	public NewOneNotePagePreviewPanel()
+	{
+		InitializeComponent();
+	}
+}
