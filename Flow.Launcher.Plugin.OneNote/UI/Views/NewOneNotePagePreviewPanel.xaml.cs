f334dd7cc21c6f5b99ee7d68bae33b5b1ffef235
Odotocodot <48138990+Odotocodot@users.noreply.github.com>
2024-06-23 14:26:20 +0100

    Remove conflicting shortcut

--- Flow.Launcher.Plugin.OneNote/UI/Views/NewOneNotePagePreviewPanel.xaml.cs
+++ Flow.Launcher.Plugin.OneNote/UI/Views/NewOneNotePagePreviewPanel.xaml.cs
@@ -32,21 +32,5 @@ namespace Flow.Launcher.Plugin.OneNote.UI.Views
             Keyboard.Focus(TextBoxPageTitle);
             TextBoxPageTitle.CaretIndex = TextBoxPageTitle.Text.Length;
         }
-
-        private void OnKeyDown(object sender, KeyEventArgs e)
-		{
-			if (e.Key != Key.Tab) 
-				return;
-		
-			if (e.Source is not (TextBox or Button)) 
-				return;
-		
-			var focusNavigationDirection = Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift)
-				? FocusNavigationDirection.Previous
-				: FocusNavigationDirection.Next;
-			((UIElement)Keyboard.FocusedElement)?.MoveFocus(new TraversalRequest(focusNavigationDirection));
-		
-			e.Handled = true;
-		}
 	}
 }
